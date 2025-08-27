f1df4b378714dcc1c7f1b6b771f6f01f64f8abe3
mosshead <garcianodianarakristy@gmail.com>
2025-08-27 10:00:11 +0800

    populated done

--- HANGARIN/hangarinorg/admin.py
+++ HANGARIN/hangarinorg/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Category, Note, Priority, Subtask, Task
+
+
+admin.site.register(Category)
+admin.site.register(Note)
+admin.site.register(Priority)
+admin.site.register(Subtask)
+admin.site.register(Task)
--- HANGARIN/hangarinorg/models.py
+++ HANGARIN/hangarinorg/models.py
@@ -1,3 +1,58 @@
 from django.db import models
 
-# Create your models here.
+class BaseModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True, db_index=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+class Priority(BaseModel):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):  
+        return self.name
+    
+class Category(BaseModel):
+    name = models.CharField(max_length=100)
+
+   
+    def __str__(self):
+        return self.name
+
+class Task(BaseModel):
+    title = models.CharField(max_length=200)
+    description = models.TextField(blank=True, null=True)
+    deadline = models.DateTimeField(blank=True, null=True)
+    status = models.CharField(
+    max_length=50, choices = [    ("Pending","Pending"), 
+         ("In Progress","In Progress"),      ("Completed","Completed")       ], default="Pending")
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True)
+    priority = models.ForeignKey(Priority, on_delete=models.SET_NULL, null=True, blank=True)
+    
+    def __str__(self):
+        return self.title
+    
+
+
+
+class Note(BaseModel):
+    task = models.ForeignKey(Task, on_delete=models.CASCADE, related_name='notes')
+    content = models.TextField()
+
+    def __str__(self):
+        return f"Note for Task: {self.task.title}"
+    
+
+class Subtask(BaseModel):
+    parent_task = models.ForeignKey(Task, on_delete=models.CASCADE, related_name='subtasks')
+    title = models.CharField(max_length=200)
+    status = models.CharField(
+    max_length=50, choices = [    ("Pending","Pending"), 
+         ("In Progress","In Progress"), ("Completed","Completed")], default="Pending")
+    
+    def __str__(self):
+        return self.title
+    
+    
+
