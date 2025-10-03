f4827291a51ec853465883c03f36e2837ad75d22
mosshead <garcianodianarakristy@gmail.com>
2025-10-04 01:00:43 +0800

    setup base.html

--- HANGARIN/hangarinorg/views.py
+++ HANGARIN/hangarinorg/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
-from hangarinorg.models import Task
+from hangarinorg.models import Task,Category
 
 class TaskListView(ListView):
     model = Task
     template_name = 'index.html'
     context_object_name = 'tasks'
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['categories'] = Category.objects.all()
+        return context
