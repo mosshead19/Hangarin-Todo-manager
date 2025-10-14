9e2413c6d0b7d20a923cbf3cd31c603048381a2d
mosshead <garcianodianarakristy@gmail.com>
2025-10-15 01:17:52 +0800

    cruds for task;lists for notes and subtasks

--- HANGARIN/hangarinorg/views.py
+++ HANGARIN/hangarinorg/views.py
@@ -3,29 +3,14 @@ from django.views.generic import ListView, CreateView  # Fixed import
 from django.views.generic.edit import  UpdateView, DeleteView  # Alternative
 from django.urls import reverse_lazy
 from hangarinorg.models import Task, Category, Subtask, Priority,Note
+from django.http import HttpResponseRedirect
+from django.shortcuts import redirect
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
-class TaskListView(ListView):
-    model = Task
-    template_name = 'index.html'
-    context_object_name = 'tasks'
-
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['categories'] = Category.objects.all()
-        return context
-    
-  #TASK CRUD VIEWS  
-class TaskCreateView(CreateView):
-    model = Task
-    fields = ['title', 'description', 'deadline', 'status', 'category', 'priority']
-    template_name = 'task_form.html'
-    success_url = reverse_lazy('dashboard')
-    
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['categories'] = Category.objects.all()
-        return context
+from django.shortcuts import redirect
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
 class TaskUpdateView(UpdateView):
     model = Task
@@ -36,7 +21,86 @@ class TaskUpdateView(UpdateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['categories'] = Category.objects.all()
+        context['priorities'] = Priority.objects.all()
+        context['notes'] = Note.objects.filter(task=self.object).order_by('-created_at')
         return context
+    
+    def post(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        
+        # Check if this is a note creation request
+        if 'add_note' in request.POST and 'note_content' in request.POST:
+            # Create a new note
+            Note.objects.create(
+                content=request.POST['note_content'],
+                task=self.object
+            )
+            # Redirect back to the same task edit page
+            return redirect('task-update', pk=self.object.pk)
+        
+        # Otherwise, handle the normal task update
+        return super().post(request, *args, **kwargs)
+
+class TaskListView(ListView):
+    model = Task
+    template_name = 'index.html'
+    context_object_name = 'tasks'
+
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['categories'] = Category.objects.all()
+        context['priorities'] = Priority.objects.all()
+        return context
+    
+  #TASK CRUD VIEWS  
+class TaskCreateView(CreateView):
+    model = Task
+    fields = ['title', 'description', 'deadline', 'status', 'category', 'priority']
+    template_name = 'index.html'
+    success_url = reverse_lazy('dashboard')
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['categories'] = Category.objects.all()
+        context['priorities'] = Priority.objects.all()
+        context['tasks'] = Task.objects.all()
+        context['notes'] = Note.objects.all()
+        return context
+    
+    def form_invalid(self, form):
+        # If form is invalid, return to same page with errors
+        context = self.get_context_data(form=form)
+        return self.render_to_response(context)
+
+class TaskUpdateView(UpdateView):
+    model = Task
+    fields = ['title', 'description', 'deadline', 'status', 'category', 'priority']
+    template_name = 'task_form.html'
+    success_url = reverse_lazy('dashboard')
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['categories'] = Category.objects.all()
+        context['priorities'] = Priority.objects.all()
+        context['notes'] = Note.objects.filter(task=self.object).order_by('-created_at')
+        return context
+    
+    def post(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        
+        # Check if this is a note creation request
+        if 'add_note' in request.POST and 'note_content' in request.POST:
+            # Create a new note
+            Note.objects.create(
+                content=request.POST['note_content'],
+                task=self.object
+            )
+            # Redirect back to the same task edit page
+            return HttpResponseRedirect(reverse_lazy('task-update', kwargs={'pk': self.object.pk}))
+        
+        # Otherwise, handle the normal task update
+        return super().post(request, *args, **kwargs)
 
 class TaskDeleteView(DeleteView):
     model = Task
@@ -60,6 +124,7 @@ class CategoryTaskListView(ListView):
         context = super().get_context_data(**kwargs)
         context['categories'] = Category.objects.all()
         context['current_category'] = Category.objects.get(id=self.kwargs['category_id'])
+        context['priorities'] = Priority.objects.all()
         return context
     
 # Category CRUD VIEWS
@@ -157,17 +222,18 @@ class NoteListView(ListView):
     model = Note
     template_name = 'note_list.html'
     context_object_name = 'notes'
+    ordering = ['-created_at']  # Show newest first
     
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['categories'] = Category.objects.all()
+        context['categories'] = Category.objects.all()  # For your sidebar
         return context
     
 class NoteCreateView(CreateView):
     model = Note
     fields = ['content']
     template_name = 'note_form.html'
-    success_url = reverse_lazy('dashboard')
+    success_url = reverse_lazy('task-update')     
     
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
