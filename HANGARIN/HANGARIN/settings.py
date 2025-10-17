ab147a7fa9e94f4b498c00f8b4f6b5146204993d
Dianara Kristy D. Garciano <142156759+mosshead19@users.noreply.github.com>
2025-10-17 15:34:28 +0800

    Update settings.py

--- HANGARIN/HANGARIN/settings.py
+++ HANGARIN/HANGARIN/settings.py
@@ -49,10 +49,7 @@ INSTALLED_APPS = [
 'allauth.socialaccount.providers.github',
 ]
 
-if "pythonanywhere" in socket.gethostname():
-    SITE_ID = 4 # production site (psusphere.pythonanywhere.com)
-else:
-    SITE_ID = 3 # local site (127.0.0.1:8000)
+SITE_ID = 4 # production site (psusphere.pythonanywhere.com)
 
 AUTHENTICATION_BACKENDS = [
 'django.contrib.auth.backends.ModelBackend',
